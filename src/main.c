210e7338d47418e44f1e4004f7e70b8b7d7bb207
Jiun Ming Hsu <hsujm219@gmail.com>
2024-03-28 20:07:17 -0300

    -

--- src/main.c
+++ src/main.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <commons/txt.h>
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[])
+{
     txt_write_in_stdout("Hola, Operativos!!\n");
     return 0;
 }
